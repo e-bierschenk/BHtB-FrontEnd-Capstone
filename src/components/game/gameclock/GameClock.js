0256a709d37570e852413de28f6ba8c15a4d5536
e-bierschenk <bushanks@gmail.com>
2022-04-28 14:49:14 -0500

    fixed clock

--- src/components/game/gameclock/GameClock.js
+++ src/components/game/gameclock/GameClock.js
@@ -1,45 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "./GameClock.css"
+
 
 export const GameClock = ({ startTime }) => {
     const [seconds, setSeconds] = useState(0)
     const [minutes, setMinutes] = useState(0)
-    const [clockUpdate, setClockUpdate] =useState(0)
-
-    let newMinutes = 0
-    let newSeconds = 0
 
 
     useEffect(() => {
-        console.log(`initializing interval`);
         const interval = setInterval(() => {
           updateTime();
         }, 1000);
       
         return () => {
-          console.log(`clearing interval`);
           clearInterval(interval);
         };
       }, []);
 
-    // Update the count down every 1 second
-    setInterval(() => {
+      function updateTime() {
+        const distance = new Date() - startTime
+        const newMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+        const newSeconds = Math.floor((distance % (1000 * 60)) / 1000).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
 
-        // Get today's date and time
-        const now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = now - startTime;
-
-        // Time calculations for days, hours, minutes and seconds
-        newMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        newSeconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        setMinutes(newMinutes.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}))
-        setSeconds(newSeconds.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}))
-
-        // setClockUpdate(clockUpdate+1)
-
-    }, 1000)
+        setMinutes(newMinutes)
+        setSeconds(newSeconds)
+      }
 
     return (<>
         <h3 className="clock">{minutes}:{seconds}</h3>
