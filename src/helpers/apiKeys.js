eaaf1d31e799659560545cbefe3adf205224c3b6
e-bierschenk <bushanks@gmail.com>
2022-05-02 11:26:41 -0500

    forgot to save changes before committing originally

--- src/helpers/apiKeys.js
+++ src/helpers/apiKeys.js
@@ -1,17 +1,9 @@
-export const imdbApi = {
-    key: "k_6bs09q45",
-    url: "https://imdb-api.com/API"
-}
-
-export const baconApi = {
-    url: "http://localhost:8088"
-}
 
 export const netlifyConfig = {
     imdbApiKey: process.env.REACT_APP_IMDB_API_KEY,
     imdbUrl: process.env.REACT_APP_AUTH_DOMAIN,
     baconUrl: REACT_APP_BACON_URL,
-    
+
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
