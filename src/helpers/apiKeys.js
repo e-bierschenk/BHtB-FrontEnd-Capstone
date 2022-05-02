100754d4217d7555ce40b3c4fc7aef17405691ba
e-bierschenk <bushanks@gmail.com>
2022-05-02 11:34:40 -0500

    added process.env to baconurl in api keys

--- src/helpers/apiKeys.js
+++ src/helpers/apiKeys.js
@@ -2,8 +2,7 @@
 export const netlifyConfig = {
     imdbApiKey: process.env.REACT_APP_IMDB_API_KEY,
     imdbUrl: process.env.REACT_APP_AUTH_DOMAIN,
-    baconUrl: REACT_APP_BACON_URL,
-
+    baconUrl: process.env.REACT_APP_BACON_URL,
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
