2205be8060daa99bd47de8c071567dc01e14f5ca
e-bierschenk <bushanks@gmail.com>
2022-05-02 15:14:30 -0500

    build error

--- src/helpers/apiKeys.js
+++ src/helpers/apiKeys.js
@@ -1,6 +1,6 @@
 
 export const netlifyConfig = {
     imdbApiKey: process.env.REACT_APP_IMDB_API_KEY,
-    imdbUrl: process.env.REACT_APP_AUTH_DOMAIN,
+    imdbUrl: process.env.REACT_APP_IMDB_URL,
     baconUrl: process.env.REACT_APP_BACON_URL,
 };
